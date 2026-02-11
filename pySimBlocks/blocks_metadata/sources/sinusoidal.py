ea31cb4f92e78a3f2856d159051e86520fb7bc86
Alessandrini Antoine <a.alsdrini@gmail.com>
2026-02-11 13:45:20 +0100

    [add] License header

--- pySimBlocks/blocks_metadata/sources/sinusoidal.py
+++ pySimBlocks/blocks_metadata/sources/sinusoidal.py
@@ -1,3 +1,23 @@
+# ******************************************************************************
+#                                  pySimBlocks
+#                     Copyright (c) 2026 Université de Lille & INRIA
+# ******************************************************************************
+#  This program is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or (at your
+#  option) any later version.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+#  for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ******************************************************************************
+#  Authors: see Authors.txt
+# ******************************************************************************
+
 from pySimBlocks.blocks_metadata.block_meta import BlockMeta
 from pySimBlocks.blocks_metadata.parameter_meta import ParameterMeta
 from pySimBlocks.blocks_metadata.port_meta import PortMeta
